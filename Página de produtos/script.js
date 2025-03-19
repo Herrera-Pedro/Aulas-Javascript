f9c14efc6e8c3f3f49ade835a899a18b3fe47d43
Pedro Henrique Herrera <161768449+Herrera-Pedro@users.noreply.github.com>
2025-03-19 20:35:37 -0300

    Initial commit

---
+++ Página de produtos/script.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+  document.querySelector(".nav-links").classList.toggle("show");
+}
+
+
+// Validar formulário
+document.getElementById("contactForm").addEventListener("submit", function (event) {
+
+
+
+});
